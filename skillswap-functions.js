4339e30790b7428eff739e19b8c5fe5938053009
Drew Jones <Drew.jones2022@gmail.com>
2026-02-15 19:05:53 -0500

    Implemented calculateTotalCost to pass tests

--- skillswap-functions.js
+++ skillswap-functions.js
@@ -6,4 +6,11 @@ function filterSkillsByCategory(skills, category) {
   return skills.filter(skill => skill.category === category);
 }
 
-module.exports = { filterSkillsByCategory };
+function calculateTotalCost(hourlyRate, hours) {
+  return hourlyRate * hours;
+}
+
+module.exports = {
+  filterSkillsByCategory,
+  calculateTotalCost
+};
